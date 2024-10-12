4510687de3f50c6407191da4ec31d67bc031bf5e
saikrishna231991 <31218759+saikrishna231991@users.noreply.github.com>
2024-10-12 15:36:31 -0500

    Update resource.tf

--- launch-ec2-instance/resource.tf
+++ launch-ec2-instance/resource.tf
@@ -1,11 +1,3 @@
-resource "aws_instance" "test" {
-  ami           - var.ami_value
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "testing"
-  }
-}
 resource "aws_s3_bucket" "s3-buucket-versioning" {
   bucket = var.s3_bucket_name
 }
