904ca73d6456af06b4e2e03d59ae768ce99edbb7
saikrishna231991 <31218759+saikrishna231991@users.noreply.github.com>
2024-10-12 15:19:21 -0500

    Create variable.tf

---
+++ launch-ec2-instance/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_value" {
+   discription = "value of ami"
+}
+
+variable "instance_type" {
+   discription = "type of ec2 instance"
+}
+variable "s3_bucket_name" {
+   discription = "name of th es3 bucket"
+}
