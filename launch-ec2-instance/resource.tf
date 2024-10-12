31e94cd8840cea5ea24331ee6b1e16c05b35f48d
saikrishna231991 <31218759+saikrishna231991@users.noreply.github.com>
2024-10-12 15:14:32 -0500

    Update resource.tf

--- launch-ec2-instance/resource.tf
+++ launch-ec2-instance/resource.tf
@@ -1,5 +1,18 @@
+variable "ami_value" {
+   discription = "value of ami"
+}
+
+variable "instance_type" {
+   discription = "type of ec2 instance"
+}
+variable "s3_bucket_name" {
+   discription = "name of th es3 bucket"
+}
+
 resource "aws_instance" "test" {
-  ami           = "ami-0fff1b9a61dec8a5f"
+  ami           - var.ami_value
+#  ami           = "ami-0fff1b9a61dec8a5f"
+  instance_type = var.instance_type
   instance_type = "t2.micro"
 
   tags = {
@@ -7,7 +20,8 @@ resource "aws_instance" "test" {
   }
 }
 resource "aws_s3_bucket" "s3-buucket-versioning" {
-  bucket = "s3-bucket-sk-89899versioning"
+  bucket = var.s3_bucket_name
+#  bucket = "s3-bucket-sk-89899versioning"
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
